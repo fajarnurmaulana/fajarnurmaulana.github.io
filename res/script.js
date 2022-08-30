0abeffa4ceb42bbc4d61c161e24195e002b8e57a
fajarnurmaulana <nengragasakti@gmail.com>
2022-08-30 11:36:17 +0700

    Ganti Lagi

--- res/script.js
+++ res/script.js
@@ -1,3 +1,16 @@
+function changeBg(){
+  let header = document.querySelector('.header')
+  let scrollValue = window.scrollY;
+  
+  if (scrollValue > 100) {
+    header.classList.add('scroll')
+  }
+  else{
+    header.classList.remove('scroll')
+  }
+}
+window.addEventListener('scroll',changeBg);
+
 function menuToggle(){
   let menu = document.querySelector('.navbar')
   menu.classList.toggle('slide')
