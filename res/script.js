6910fc7806529eb4b70400397f451de231680ea5
Fajar Nur Maulana <88777431+fajarnurmaulana@users.noreply.github.com>
2022-08-30 10:08:56 +0700

    Delete res directory
    Change All Source Code

--- res/script.js
+++
@@ -1,6 +0,0 @@
-const menuToogle = document.querySelector('.hamburger input');
-const menu = document.querySelector('.navlink');
-
-menuToogle.addEventListener('click', function () {
-    menu.classList.toggle('slide');
-})
